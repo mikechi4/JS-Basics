7742cd2464ee1c73d2178546b62013aa0031e8cf
Mike <mike.chi4@gmail.com>
2016-09-16 16:41:08 -0600

    Finished problems

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -69,15 +69,18 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-  
+  function myName(){
+  	return "Mike";
+  }
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
+  var newMyName = myName;
 
 //Now alert the result of invoking newMyName
-
+	alert(newMyName());
 
 
 //////////////////PROBLEM 7////////////////////
@@ -87,9 +90,16 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
+  function outerFn(){
+  	return function(){
+  		return "Mike";
+  	}
+  }
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
+  var innerFn = outerFn();
 
 //Now invoke innerFn.
+	innerFn();
